21ca72ef1129703453cb84db1aa047d6f6db6c36
Prince179 <Prince1794.be21@chikara.edu.in>
2024-08-27 18:02:09 +0530

    single_img

--- client/blogBuzz/src/pages/Menu.jsx
+++ client/blogBuzz/src/pages/Menu.jsx
@@ -1,6 +1,7 @@
 import React ,{useEffect,useState}from 'react'
 import axios from 'axios';
 import conf from '../conf';
+import Image from '../components/Image';
 
 const Menu = ({cat}) => {
   const [posts,setPosts] =useState([]);
@@ -21,7 +22,7 @@ const Menu = ({cat}) => {
       <h1> Other posts you may like</h1>
       {posts.map((post)=>(
         <div className="post" key={post.id}>
-            <img src={`../uploads/${post.img}`} alt=''></img>
+          <Image url={post.img}></Image>
             <h2>{post.title}</h2>
             <button> Read More</button>
         </div>
